4c955e2231fa0779b8845fa06c3a0bb79ef6703c
Leandro Reis <leandro.reiz@gmail.com>
2022-10-06 21:43:41 +0100

    exercise 2 - 06-10-22

--- programming_and_algorithms/pa1_ex1.py
+++ programming_and_algorithms/pa1_ex1.py
@@ -1,5 +1,5 @@
 # get user input
-incoming_int_as_string = input("Enter an integer to convert: ")
+incoming_int_as_string = input("Enter an integer to convert to binary: ")
 
 # validate information entered by user
 if incoming_int_as_string.isnumeric():
@@ -22,3 +22,22 @@ while incoming_int > 0:
     binary_string = str(remainder) + binary_string  # convert and add the remainder at the beginning of the string
 
 print(f"Result: {binary_string}")
+
+# Get a binary number and convert it to an integer
+binary_string = input("Enter a binary to be converted to integer: ")
+
+for char in binary_string:
+    if char not in ('0', '1'):
+        print(f"Invalid input. '{char}' is not a valid value! Binaries are made of 0s and 1s only.")
+        quit(1)
+
+temp_binary_string = binary_string
+power = 0
+sum = 0
+
+while len(temp_binary_string) > 0:
+    sum += int(temp_binary_string[-1]) * (2 ** power)
+    temp_binary_string = temp_binary_string[:-1]
+    power += 1
+
+print(f"Returned integer from binary: {sum}")
